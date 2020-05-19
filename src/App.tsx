3ce832273e87a901e04af3861612d7358ac61213
Utkarsh Maharana <utkarshm27@gmail.com>
2020-05-19 21:01:11 +0530

    setting up funnel chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./assets/css/main.css";
+import "./assets/css/theme.css";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  componentDidMount() {
+    let graph = new FunnelGraph({
+      container: "#funnel",
+      gradientDirection: "horizontal",
+      data: {
+        labels: ["Impressions", "Add To Cart", "Buy"],
+        colors: ["orange", "red"],
+        values: [12000, 5700, 360],
+        gradientDirection: "horizontal",
+      },
+      displayPercent: true,
+      direction: "horizontal",
+    });
+
+    graph.draw();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Funnel Chart Demo</h1>
+        <div
+          id="funnel"
+          style={{
+            height: "326.5px",
+            width: "640px",
+            background: "#393862",
+            padding: "20px",
+          }}
+        ></div>
+      </div>
+    );
+  }
 }
-
-export default App;
